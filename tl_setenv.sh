114fe26618ea6b9d6237b05ad8ce14d63b1f9d31
Yi Qingliang <niqingliang2003@tom.com>
2012-08-31 03:22:42 +0000

    add script for setting texlive env

---
+++ tl_setenv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Author: Yi Qingliang
+# this file is used for setting environment of the texlive.
+
+if [ "$0" == "${BASH_SOURCE}" ]; then
+	echo "ERROR: you need source this file, but not execute it directly!!!"
+	exit 1
+fi
+
+TL_MAIN_DIR="`readlink -fe "${BASH_SOURCE}" | xargs dirname`"
+
+PATH=${TL_MAIN_DIR}/bin/i386‐linux:$PATH; export PATH
+MANPATH=${TL_MAIN_DIR}/texmf/doc/man:$MANPATH; export MANPATH
+INFOPATH=${TL_MAIN_DIR}/texmf/doc/info:$INFOPATH; export INFOPATH
