23360e9fd70757d69a0140b383b4b6afa2af8ac7
Yi Qingliang <niqingliang2003@tom.com>
2012-08-31 08:02:35 +0000

    add comment for adobe fonts under ArchLinux

--- tl_setenv.sh
+++ tl_setenv.sh
@@ -13,4 +13,11 @@ TL_MAIN_DIR="`readlink -fe "${BASH_SOURCE}" | xargs dirname`"
 PATH=${TL_MAIN_DIR}/bin/x86_64-linux:$PATH; export PATH
 MANPATH=${TL_MAIN_DIR}/texmf/doc/man:$MANPATH; export MANPATH
 INFOPATH=${TL_MAIN_DIR}/texmf/doc/info:$INFOPATH; export INFOPATH
+# on ArchLinux, the adobe font pkgs in aur will generate two dirs under
+# /usr/share/fonts:
+# adobe | Adobe
+# but the context can only use the 'adobe', can't access 'Adobe'
+# maybe the capital problem, context may ignored the 'Adobe',
+# so we should put all Adobe fonts in another directory, and
+# add it in the env var 'OSFONTDIR'.
 OSFONTDIR="/usr/share/fonts:$OSFONTDIR"; export OSFONTDIR
