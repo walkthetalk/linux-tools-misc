1f117e89d57b23d6db5f8dc82f09be223c169c7a
Yi Qingliang <niqingliang2003@gmail.com>
2018-01-15 13:32:42 +0800

    update sync_yocto
    Signed-off-by: Yi Qingliang <niqingliang2003@gmail.com>

--- yocto/sync_yocto.py
+++ yocto/sync_yocto.py
@@ -47,13 +47,7 @@ class creg_file:
 		return self._name
 
 class csub_rep:
-	def __init__(self, base_url, loc_dir):
-		self._main_page = base_url
-		self._fl_non_regular = {}
-		self._fl_regular = {}
-		self._fl_dl = {}
-		self._fl_ordered = []
-		self._loc_dir = loc_dir
+	def __genlist(self):
 		# get index
 		#call("wget --progress=bar -O " + "index.html" + " " + base_url, shell=True)
 		soup = BeautifulSoup(open("index.html"), "html.parser")
@@ -140,6 +134,98 @@ class csub_rep:
 		# the final one
 		if prev_file:
 			self._fl_regular[prev_file.name()] = prev_file
+	def __genlist2(self):
+		# get index
+		#call("wget --progress=bar -O " + "index.html" + " " + base_url, shell=True)  
+		fp = open("index.html")
+
+		prev_file = None
+		prev_pkg_name = ""
+		prev_pkg_ver = ""
+		for cnt, line in enumerate(fp):
+			link = re.split(' +', line)
+
+			file_name = link[8]
+			file_size = link[4]
+			file_time = time.strptime(link[7] + '-' + link[5] + '-' + link[6] + ' 00:00', '%Y-%b-%d %H:%M')
+			#directory
+			if re.match(".*/$", file_name):
+				continue
+			#done file
+			if re.match(".*\.done$", file_name):
+				continue
+			if re.match(".*\.lock$", file_name):
+				continue
+			if re.match(".*bad-checksum.*", file_name):
+				continue
+			if file_size == 0:
+				continue
+
+			# parse
+			self._fl_ordered.append(file_name)
+			if re.match(r"^.*"
+				"(_\.svn|\.trunk|svn\.)"
+				".*$", file_name):
+				self._fl_non_regular[file_name] = creg_file(self, file_name, file_size, file_time, False)
+				continue
+			if re.match(r"^git[2]?_.*$", file_name):
+				self._fl_non_regular[file_name] = creg_file(self, file_name, file_size, file_time, False)
+				continue
+			if re.match(r"^.*\.patch$", file_name):
+				self._fl_non_regular[file_name] = creg_file(self, file_name, file_size, file_time, True)
+				continue
+			m = re.match(r"^(?P<pkgname>.*)"
+				"(-|_|-s|\.v)"
+				"(?P<pkgver>(([0-9]+\.)+[0-9]+)|(([0-9]+_)+[0-9]+))"
+				"(?P<stage>-P[0-9]+|p[0-9]+|[a-z]|alpha|-?beta[0-9]*|-?rc[0-9]+|-pre[0-9]+)?"
+				"(?P<revision>-[0-9]+)?"
+				"\."
+				"(?P<ext>tar\.bz2|tar\.gz|tar\.xz|tgz|zip|gz|bz2)"
+				"$", file_name)
+			if not m:
+				self._fl_non_regular[file_name] = creg_file(self, file_name, file_size, file_time, True)
+				continue
+
+			# regular file
+			self._fl_regular[file_name] = None
+			pkgname = m.group("pkgname")
+			pkgver = m.group("pkgver")
+			if pkgname != prev_pkg_name: # different pkg
+				if prev_file:
+					#print(prev_pkg_name, prev_pkg_ver)
+					self._fl_regular[prev_file.name()] = prev_file
+				# update prev
+				prev_pkg_name = pkgname
+				prev_pkg_ver = pkgver
+				prev_file = creg_file(self, file_name, file_size, file_time, True)
+
+			else: # same pkg
+				if prev_pkg_ver == pkgver:
+					None
+				else:
+					prev_ver_array = re.split('[\._]', prev_pkg_ver)
+					ver_array = re.split('[\._]', pkgver)
+					for i,j in zip(prev_ver_array, ver_array):
+						pn = int(i)
+						cn = int(j)
+						if pn == cn:
+							continue
+						if pn < cn:
+							prev_pkg_ver = pkgver
+							prev_file = creg_file(self, file_name, file_size, file_time, True)
+						break
+		# the final one
+		if prev_file:
+			self._fl_regular[prev_file.name()] = prev_file
+	def __init__(self, base_url, loc_dir):
+		self._main_page = base_url
+		self._fl_non_regular = {}
+		self._fl_regular = {}
+		self._fl_dl = {}
+		self._fl_ordered = []
+		self._loc_dir = loc_dir
+		
+		self.__genlist2()
 		# remove files non't need dl in non-regular list
 		with fileinput.input(files=("yocto-nrfl-with-flag.lst")) as f:
 			for line in f:
