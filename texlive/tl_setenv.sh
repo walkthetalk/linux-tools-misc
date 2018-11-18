14747aafe918bab4da5fc6b737248a71b8558936
Yi Qingliang <niqingliang2003@tom.com>
2018-11-18 11:17:25 +0800

    tl_setenv.sh: add multi os support

--- texlive/tl_setenv.sh
+++ texlive/tl_setenv.sh
@@ -4,13 +4,26 @@
 # this file is used for setting environment of the texlive.
 
 if [ "$0" == "${BASH_SOURCE}" ]; then
-	echo "ERROR: you need source this file, but not execute it directly!!!"
-	exit 1
+        echo "ERROR: you need source this file, but not execute it directly!!!"
+        exit 1
 fi
 
 TL_MAIN_DIR="`readlink -fe "${BASH_SOURCE}" | xargs dirname`"
+TL_MAIN_DIR="${TL_MAIN_DIR}/2018"
 
-PATH=${TL_MAIN_DIR}/bin/x86_64-linux:$PATH; export PATH
+unameOut="`uname -s`"
+case "${unameOut}" in
+        MINGW64_NT*)
+                PATH=${TL_MAIN_DIR}/bin/win32:$PATH
+                OSFONTDIR="/c/windows/fonts:$OSFONTDIR"
+                ;;
+        *)
+                PATH="${TL_MAIN_DIR}/bin/x86_64-linux:$PATH"
+                OSFONTDIR="/usr/share/fonts:$OSFONTDIR"
+                ;;
+esac
+
+export PATH
 MANPATH=${TL_MAIN_DIR}/texmf/doc/man:$MANPATH; export MANPATH
 INFOPATH=${TL_MAIN_DIR}/texmf/doc/info:$INFOPATH; export INFOPATH
 # on ArchLinux, the adobe font pkgs in aur will generate two dirs under
@@ -20,4 +33,4 @@ INFOPATH=${TL_MAIN_DIR}/texmf/doc/info:$INFOPATH; export INFOPATH
 # maybe the capital problem, context may ignored the 'Adobe',
 # so we should put all Adobe fonts in another directory, and
 # add it in the env var 'OSFONTDIR'.
-OSFONTDIR="/usr/share/fonts:$OSFONTDIR"; export OSFONTDIR
+export OSFONTDIR
