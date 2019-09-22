aaca8fd40d2998e277981ce726c27eeb9386a816
Yi Qingliang <niqingliang2003@gmail.com>
2019-09-22 17:12:12 +0800

    sync_yocto: download speed too slow, use axel against wget
    Signed-off-by: Yi Qingliang <niqingliang2003@gmail.com>

--- yocto/sync_yocto.py
+++ yocto/sync_yocto.py
@@ -9,22 +9,34 @@ import re
 import os
 import tempfile
 import subprocess
+from multiprocessing import Queue
+import threading
+#from threading import Thread
 
 #from bs4 import BeautifulSoup
 from subprocess import call
 import fileinput
 import urllib.parse
 
-# the `-N' option can make wget overwrite the old file
-def g_download(rem_file, loc_dir):
-	call("wget -e robots=off -N --timeout=10 --wait=10 --tries=0 --progress=bar -P " + loc_dir + " " + rem_file, shell=True)
-
-g_cmptimefmt = "%Y-%b-%d"
 g_splitReg = re.compile("^<a href=\"([^>\"]+)\">[^</]+</a> +([^ ]+) ([^ ]+) +([1-9][0-9]*)$")
 g_list_file = "files.lst"
 g_html_file = "index.html"
 g_loc_dir = './sources.yoctoproject/'
+g_new_dir = './sources.new/'
 g_deprecated_dir = "./sources.deprecated/"
+g_thread_number = 10
+
+# the `-N' option can make wget overwrite the old file
+def g_download(rem_file, loc_dir):
+	#call("wget -e robots=off -N --timeout=10 --wait=10 --tries=0 --progress=bar -P " + loc_dir + " " + rem_file, shell=True)
+	cmd = "axel " \
+		+ "-n " + "32" + " " \
+		+ "-a -v " \
+		+ "-U " + "'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1 Safari/605.1.15'" + " " \
+		+ "-o '" + loc_dir + "' " \
+		+ rem_file
+	#print(cmd)
+	call(cmd, shell=True)
 
 # https://stackoverflow.com/questions/4836710/does-python-have-a-built-in-function-for-string-natural-sort
 def natural_sorted(iterable, key=None, reverse=False):
@@ -73,13 +85,25 @@ class creg_file:
 		if not self.get_need():
 			return False
 		file_path = self._parent.loc_dir() + self._name
+		
+		if os.path.exists(file_path + ".st"):
+			os.remove(file_path)
+			os.remove(file_path + ".st")
 		if os.path.exists(file_path):
-			ltime = time.gmtime(os.path.getmtime(file_path))
-			if time.strftime(g_cmptimefmt, ltime) != time.strftime(g_cmptimefmt, self._time):
-				#print(self._name + " local time: " + time.strftime(g_cmptimefmt, ltime) + ", remote time: " + time.strftime(g_cmptimefmt, self._time))
-				return True
-			else:
+			statinfo = os.stat(file_path)
+			if (statinfo.st_size == self._size) and (time.gmtime(statinfo.st_mtime) >= self._time):
 				return False
+			#print("need dl " + self._name)
+			#print("local size: ")
+			#print(statinfo.st_size)
+			#print("remote size: ")
+			#print(self._size)
+			#print(statinfo.st_size == self._size)
+			#print("local time: ")
+			#print(time.gmtime(statinfo.st_mtime))
+			#print("remote time: ")
+			#print(self._time)
+			#print(time.gmtime(statinfo.st_mtime) >= self._time)
 		return True
 	def download(self):
 		if self.need_dl():
@@ -108,7 +132,7 @@ class csub_rep:
 				continue
 			if re.match("^git2_tmp.*", file_name):
 				continue
-			file_size = splitR.group(4)
+			file_size = int(splitR.group(4))
 			file_time = time.strptime(splitR.group(2) + " " + splitR.group(3), "%d-%b-%Y %H:%M")
 
 			self._full_fl[file_name] = creg_file(self, file_name, file_size, file_time)
@@ -127,19 +151,40 @@ class csub_rep:
 					if m.group(1) == "+" and m.group(2) in self._full_fl:
 						self._full_fl[m.group(2)].set_need(True)
 
+		self._dl_fl = {}
+		for (k,v) in natural_sorted(self._full_fl.items()):
+			if v.need_dl():
+				self._dl_fl[k] = v
+
 		# create locale directory
 		if not os.path.exists(self._loc_dir):
 			os.makedirs(self._loc_dir)
+
 	def loc_dir(self):
 		return self._loc_dir
 	def main_page(self):
 		return self._main_page
 
 	def download(self):
-		for (k, v) in natural_sorted(self._full_fl.items()):
-			v.download()
+		self._queue = Queue(len(self._dl_fl))
+		for (k, v) in natural_sorted(self._dl_fl.items()):
+			self._queue.put(self.main_page() + k)
+			#g_download(fl, self.loc_dir())
+		self._locker = threading.Lock()
 
 		return
+	def run(self, threadName):
+		while True:
+			self._locker.acquire()
+			if not self._queue.empty():
+				data = self._queue.get()
+				self._locker.release()
+				print("thread {} processing {}".format(threadName, data))
+				g_download(data, self.loc_dir())
+			else:
+				self._locker.release()
+				break
+			time.sleep(1)
 	def save(self):
 		with open(g_list_file + ".new", 'w') as f:
 			for (k, v) in natural_sorted(self._full_fl.items()):
@@ -149,9 +194,10 @@ class csub_rep:
 		cur_files = os.listdir(self._loc_dir)
 		for i in cur_files:
 			if (not (i in self._full_fl)) or (not self._full_fl[i].get_need()):
-				#print("mv " + self._loc_dir + i + " " + g_deprecated_dir)
-				call("mv " + self._loc_dir + i + " " + g_deprecated_dir)
-				os.remove(self._loc_dir + i)
+				#print("mving " + i)
+				print("mv " + self._loc_dir + i + " " + g_deprecated_dir)
+				call("mv " + self._loc_dir + i + " " + g_deprecated_dir, shell=True)
+				#os.remove(self._loc_dir + i)
 
 		return
 
@@ -160,6 +206,14 @@ if __name__ == '__main__':
 	#call("wget --progress=bar -O " + g_html_file + " " + base_url, shell=True)
 	test = csub_rep(base_url, g_loc_dir)
 	test.download()
+
+	threadList = []
+	for i in range(0, g_thread_number):
+		tmp = threading.Thread(target = test.run, args = [i])
+		tmp.start()
+		threadList.append(tmp)
+	for i in range(0, g_thread_number):
+		threadList[i].join()
 	test.save()
 	test.rm_old_files()
 	call("rm ./sources/*; find sources.yoctoproject/ sources.custom/ -type f -exec ln -sf ../{} ./sources/ \\;", shell=True)
